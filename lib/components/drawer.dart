38551dd54a61267b870f141bbd8c212c086b90ad
fengjiongmax <fengjiongmax@gmail.com>
2020-07-06 18:58:48 +0800

    display user avatar

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -11,59 +11,57 @@ import 'package:beamu/routes/login.dart';
 import 'package:beamu/routes/organization_list.dart';
 import 'package:beamu/routes/repositories_list.dart';
 
-class BeamuDrawer extends StatefulWidget{
+class BeamuDrawer extends StatefulWidget {
   @override
   _BeamuDrawerState createState() => _BeamuDrawerState();
 }
 
-class _BeamuDrawerState extends State<BeamuDrawer>{
-  bool _avatarSet = false;
-
+class _BeamuDrawerState extends State<BeamuDrawer> {
   @override
-  void initState(){
-    if(config.avatar == null){
-      getUserInfo().then((v){
+  void initState() {
+    if (config.avatar == null) {
+      getUserInfo().then((v) {
         config.setUserAvatar(v.avatarUrl);
-        if(this.mounted){
-          setState(() {
-                    _avatarSet = true;
-          });
+        if (this.mounted) {
+          setState(() {});
         }
       });
     }
-    setState(() {
-      _avatarSet = config.avatar != null;
-    });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+//    print("build drawer:${config.avatar}");
     return Drawer(
       child: ListView(
         children: <Widget>[
           DrawerHeader(
             child: Column(
               children: <Widget>[
-                SizedBox(height: 80,),
+                SizedBox(
+                  height: 80,
+                ),
                 ListTile(
-                  leading: _avatarSet?Image.network(config.avatar,height: 50,width: 50,fit: BoxFit.scaleDown,):Icon(Icons.account_circle),
+                  leading: config.avatar != null
+                      ? Image.network(config.avatar,
+                          height: 50, width: 50, fit: BoxFit.scaleDown)
+                      : Icon(Icons.account_circle),
                   title: Text(config.userName),
                 )
               ],
             ),
-            decoration: BoxDecoration(
-              color: Colors.blue
-            ),
+            decoration: BoxDecoration(color: Colors.blue),
           ),
           ListTile(
             leading: Icon(Icons.book),
             title: Text('Repositories'),
             onTap: () {
               Navigator.of(context).pop();
-              Navigator.pushReplacement(context,
-               MaterialPageRoute(builder: (BuildContext context) =>RepositoriesList())
-              );
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => RepositoriesList()));
             },
           ),
           ListTile(
@@ -71,9 +69,10 @@ class _BeamuDrawerState extends State<BeamuDrawer>{
             title: Text('Organizations'),
             onTap: () {
               Navigator.of(context).pop();
-              Navigator.pushReplacement(context,
-               MaterialPageRoute(builder: (BuildContext context) =>OrganizationList())
-              );
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => OrganizationList()));
             },
           ),
           // ListTile(
@@ -90,16 +89,19 @@ class _BeamuDrawerState extends State<BeamuDrawer>{
 
           //   },
           // ),
-          Divider(color: Colors.black,),
+          Divider(
+            color: Colors.black,
+          ),
           ListTile(
             leading: Icon(Icons.exit_to_app),
             title: Text('Logout'),
-            onTap: () async{
+            onTap: () async {
               await logout();
               Navigator.of(context).pop();
-              Navigator.pushReplacement(context,
-               MaterialPageRoute(builder: (BuildContext context) =>Login())
-              );
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => Login()));
             },
           ),
         ],
--- lib/share/authorizations.dart
+++ lib/share/authorizations.dart
@@ -13,91 +13,99 @@ import 'package:beamu/model/token_model.dart';
 import 'package:beamu/share/configs.dart';
 import 'package:beamu/share/requests.dart';
 
-enum LOGIN_STAT{ failed,success,invalid_host,timeout}
+enum LOGIN_STAT { failed, success, invalid_host, timeout }
 
-Future<LOGIN_STAT> loginAction(String username,String password,String giteaHost) async{
-  while(giteaHost.substring(giteaHost.length-1,giteaHost.length) == '/'){
-    giteaHost = giteaHost.substring(0,giteaHost.length-1);//remove the last '/'
+Future<LOGIN_STAT> loginAction(
+    String username, String password, String giteaHost) async {
+  while (giteaHost.substring(giteaHost.length - 1, giteaHost.length) == '/') {
+    giteaHost =
+        giteaHost.substring(0, giteaHost.length - 1); //remove the last '/'
   }
   var retVal = LOGIN_STAT.failed;
-  var loginUrl = giteaHost+"/api/v1/users/"+username+"/tokens";
-  var loginCredential = username+":"+password;
-  Map<String,String> header = new Map<String,String>();
+  var loginUrl = giteaHost + "/api/v1/users/" + username + "/tokens";
+  var loginCredential = username + ":" + password;
+  Map<String, String> header = new Map<String, String>();
   const base64 = Base64Codec();
   var authentication = base64.encode(utf8.encode(loginCredential));
-  header['Authorization'] = "Basic "+ authentication;
+  header['Authorization'] = "Basic " + authentication;
 
-  doLogin() async{
-    Response rtVal ;
-    final response = await httpGet(loginUrl,header).timeout(Duration(seconds: 30),onTimeout: (){
-      return null;
-    });
-    rtVal = response;
-    return rtVal;
-  }
-
-  createToken() async{
-    header['content-type'] = "application/json";
-    String postBody = '{"name":"'+APP_NAME+'"}';
+  doLogin() async {
     Response rtVal;
-    final response = await httpPost(loginUrl,postBody,header).timeout(Duration(seconds: 30),onTimeout: (){
+    final response = await httpGet(loginUrl, header)
+        .timeout(Duration(seconds: 30), onTimeout: () {
       return null;
     });
     rtVal = response;
     return rtVal;
   }
-  
-  await doLogin().then((value) async{
-    if(value != null){
-     if(value.statusCode==200){
-          retVal = LOGIN_STAT.success;
-          SharedPreferences prefs = await SharedPreferences.getInstance();
-          await prefs.setBool(DONE_LOGIN, true);
-          await config.saveValue(GITEA_HOST, giteaHost);
-          await config.saveValue(USERNAME, username);
-          await config.saveValue(PASSWORD, password);
 
-          //parse response
-          var parsed = json.decode(value.body).cast<Map<String,dynamic>>();
-          List<TokenModel> tokens = parsed.map<TokenModel>((json)=> TokenModel.fromJson(json)).toList();
+  createToken() async {
+    header['content-type'] = "application/json";
+    String postBody = '{"name":"' + APP_NAME + '"}';
+    Response rtVal;
+    final response = await httpPost(loginUrl, postBody, header)
+        .timeout(Duration(seconds: 30), onTimeout: () {
+      return null;
+    });
+    rtVal = response;
+    return rtVal;
+  }
 
-          TokenModel token ;
-          if(tokens.length>0){
-            token = tokens.firstWhere((w){
-              return w.name==APP_NAME;
-            });
-          }
-          
-          if(token == null){//not found,create one
-            var response = await createToken();
-            token = TokenModel.fromJson(json.decode(response.body));
-          }
-          await config.setToken(token.sha1);
-      }else if(value.statusCode==401){
-            retVal = LOGIN_STAT.failed;
-      }else{// this includes 404
-            retVal = LOGIN_STAT.invalid_host;
+  await doLogin().then((value) async {
+    if (value != null) {
+      if (value.statusCode == 200) {
+        retVal = LOGIN_STAT.success;
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setBool(DONE_LOGIN, true);
+        await config.saveValue(GITEA_HOST, giteaHost);
+        await config.saveValue(USERNAME, username);
+        await config.saveValue(PASSWORD, password);
+
+        //parse response
+        var parsed = json.decode(value.body).cast<Map<String, dynamic>>();
+        List<TokenModel> tokens = parsed
+            .map<TokenModel>((json) => TokenModel.fromJson(json))
+            .toList();
+
+        TokenModel token;
+        if (tokens.length > 0) {
+          token = tokens.firstWhere((w) {
+            return w.name == APP_NAME;
+          });
+        }
+
+        if (token == null) {
+          //not found,create one
+          var response = await createToken();
+          token = TokenModel.fromJson(json.decode(response.body));
+        }
+        await config.setToken(token.sha1);
+      } else if (value.statusCode == 401) {
+        retVal = LOGIN_STAT.failed;
+      } else {
+        // this includes 404
+        retVal = LOGIN_STAT.invalid_host;
       }
-    }else{
+    } else {
       retVal = LOGIN_STAT.timeout;
     }
   });
   return retVal;
 }
 
-Future<UserModel> getUserInfo() async{
-  var user = await getUser(config.userName);
+Future<UserModel> getUserInfo() async {
+  var user = await getUser(config.userName.trim());
   return user;
 }
 
-Future<bool> loginCHK() async{
+Future<bool> loginCHK() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
-  return prefs.getBool(DONE_LOGIN) && await config.setLoginToken(); // find token locally
+  return prefs.getBool(DONE_LOGIN) &&
+      await config.setLoginToken(); // find token locally
 }
 
-Future<bool> logout() async{
+Future<bool> logout() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   prefs.clear();
   return await config.clearAll();
 }
-
